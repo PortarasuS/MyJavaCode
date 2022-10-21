ea4e45cb48619fe7292dfd77b07d3f70d87b4a56
PortarasuS <111957586+PortarasuS@users.noreply.github.com>
2022-10-21 23:20:03 +0530

    My First java Code

---
+++ Alphabets.java
@@ -0,0 +1,48 @@
+
+public class Alphabets 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		int n = 10;
+		for (int i=0; i<n ; i++)
+		{
+			for (int j=0; j<n ; j++)
+			{
+				if (i ==0 && j<=n/2 || i <= n && j==0 || i <=(n-1)/2 && j==(n)/2 || i==n/2 && j<= n/2 )
+				{
+					System.out.print ("*");
+				}
+				else 
+				{
+					System.out.print(" ");
+
+				}
+			}
+			
+		System.out.println();
+		}
+		
+
+		for (int i=0; i<n ; i++)
+		{
+			for (int j=0; j<n ; j++)
+			{
+				if (i ==0 && j<=n/2 || i <= n && j==0 || i <=(n-1)/2 && j==(n)/2 || i==n/2 && j<= n/2 )
+				{
+					System.out.print ("*");
+				}
+				else 
+				{
+					System.out.print(" ");
+
+				}
+			}
+			
+		System.out.println();
+		}
+	}
+
+}
